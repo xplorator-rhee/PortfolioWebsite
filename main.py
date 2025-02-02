ede0b7896249796d4146bbd228453e019f3fe1be
unknown <ryan.rhee315@outlook.com>
2025-02-02 12:06:23 -0600

    Adding response HTML

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
