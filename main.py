b3c0c5e894043d3c730e8f3ebf0e96984d6797a5
unknown <ryan.rhee315@outlook.com>
2025-02-02 20:21:44 -0600

    Starting development of API

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+blogs = [
+    {"id": 1,
+     "title": "Blueprints: The journey of self-taught full-stack"
+     "date": "2025-02-02 20:17"
+     "content": "Hello World!"
+     }
+]
 
 app = FastAPI()
+api_router = APIRouter()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/", response_class=HTMLResponse)
-async def root():
-    return HTMLResponse("/static/Static1.html", status_code=200)
+templates = Jinja2Templates(directory="templates")
+
+@api_router.get("/", status_code=200)
+def root() -> dict:
+    return {"msg": "Hello World"}
+
+@api_router.get("/musings/{blog_id}", status_code=200)
+def fetch_blog(*, blog_id: int) -> dict:
+    for blog in blogs:
+        if blog["id"] == blog_id:
+            result = blog
+    return blog[0]
+
+app.include_router(api_router)
